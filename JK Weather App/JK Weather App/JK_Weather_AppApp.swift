567abd2db553b8a079a838b44a7f9196f48aa156
Janak Khadka <janakkhadka08@gmail.com>
2025-02-18 21:58:25 +0545

    project created

---
+++ JK Weather App/JK Weather App/JK_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JK_Weather_AppApp.swift
+//  JK Weather App
+//
+//  Created by Janak Khadka on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct JK_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JK Weather App/JK Weather AppTests/JK_Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JK_Weather_AppTests.swift
+//  JK Weather AppTests
+//
+//  Created by Janak Khadka on 18/02/2025.
+//
+
+import Testing
+@testable import JK_Weather_App
+
+struct JK_Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
